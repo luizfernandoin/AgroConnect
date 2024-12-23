56c31e7abcc2f4fdaa748c6b5d09e0abd754f78f
luizfernandoin <luizfernandoinfo100@gmail.com>
2024-12-23 13:19:13 -0300

    Inicializando spring

---
+++ server/agroconnect/src/main/java/com/campus_mobile/agroconnect/AgroconnectApplication.java
@@ -0,0 +1,13 @@
+package com.campus_mobile.agroconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroconnectApplication.class, args);
+	}
+
+}
---
+++ server/agroconnect/src/test/java/com/campus_mobile/agroconnect/AgroconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus_mobile.agroconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
