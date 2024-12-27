b30c8b5df82a878c0581185d4046f232190c7abb
luizfernandoin <luizfernandoinfo100@gmail.com>
2024-12-27 10:23:43 -0300

    feat: criando SecurityFilterChain

---
+++ server/agroconnect/src/main/java/com/campus_mobile/agroconnect/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.campus_mobile.agroconnect.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
--- server/agroconnect/src/main/resources/db/migration/V6__create-table-categories.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE categories (
-    id_product UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    category VARCHAR(255) NOT NULL,
-    CONSTRAINT primary_key_categories PRIMARY KEY (id_product, category),
-    CONSTRAINT fk_product FOREIGN KEY (id_product) REFERENCES product(id)
-);
