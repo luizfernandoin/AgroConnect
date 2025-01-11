47897af680de182f370317383b5a1434ebc2c6c3
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-01-11 00:36:43 -0300

    feat: adicionando rota de users

--- server/agroconnect/src/main/java/com/campus_mobile/agroconnect/security/SecurityConfig.java
+++ server/agroconnect/src/main/java/com/campus_mobile/agroconnect/security/SecurityConfig.java
@@ -30,10 +30,11 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/api/users/all").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/files/*").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
