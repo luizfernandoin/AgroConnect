2a18222003cad189ae5ff811863bc3c5112f3e2b
tatiany <tatianysouza2018@gmail.com>
2025-02-02 15:11:26 -0300

    refactor: deixando pesquisa funcional

--- client/screens/ResultadoPesquisa.js
+++ client/screens/ResultadoPesquisa.js
@@ -23,36 +23,44 @@ const ResultadoPesquisa = ({ route, navigation }) => {
   const [minPrice, setMinPrice] = useState('');
   const [maxPrice, setMaxPrice] = useState('');
   const [selectedCategory, setSelectedCategory] = useState([]);
-  const [selectedLocation, setSelectedLocation] = useState('');
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   // Dados simulados dos produtos
   const allProducts = [
-    { id: '1', image: require('../assets/produto.jpg'), title: 'Café', price: '17.89', user: 'João Silva', category: 'Bebidas', location: 'São Paulo' },
-    { id: '2', image: require('../assets/produto.jpg'), title: 'Feijão', price: '12.50', user: 'Maria Oliveira', category: 'Grãos', location: 'Rio de Janeiro' },
-    { id: '3', image: require('../assets/produto.jpg'), title: 'Arroz', price: '4.99', user: 'Carlos Santos', category: 'Grãos', location: 'São Paulo' },
+    { id: '1', image: require('../assets/produto.jpg'), title: 'Café', price: 'R$ 17,89', description: 'Café 100% arábica, torrado e moído, ideal para um café fresco e encorpado.', producer: 'Produtor A', category: 'Bebidas', nota: 4.6 },
+    { id: '2', image: require('../assets/produto.jpg'), title: 'Feijão', price: 'R$ 12,50', description: 'Feijão carioca selecionado, rico em proteínas e perfeito para refeições saudáveis.', producer: 'Produtor B', category: 'Grãos', nota: 3.9 },
+    { id: '3', image: require('../assets/produto.jpg'), title: 'Arroz', price: 'R$ 4,99', description: 'Arroz branco tipo 1, grãos inteiros e soltinhos para suas receitas.', producer: 'Produtor C', category: 'Grãos', nota: 4.2 },
+    { id: '4', image: require('../assets/produto.jpg'), title: 'Milho', price: 'R$ 3,50', description: 'Milho seco para pipoca ou uso em receitas diversas.', producer: 'Produtor J', category: 'Grãos', nota: 4.3 },
+    { id: '5', image: require('../assets/produto.jpg'), title: 'Jaca', price: 'R$ 13,50', description: 'Jaca madura e fresca, com sabor doce e polpa suculenta.', producer: 'Produtor D', category: 'Frutas', nota: 4.5 },
+    { id: '6', image: require('../assets/produto.jpg'), title: 'Manga', price: 'R$ 6,00', description: 'Manga doce e aromática, rica em vitamina C e fibras.', producer: 'Produtor E', category: 'Frutas', nota: 4.7 },
+    { id: '7', image: require('../assets/produto.jpg'), title: 'Goiaba', price: 'R$ 5,00', description: 'Goiaba fresca e nutritiva, ótima para consumo in natura ou em doces.', producer: 'Produtor F', category: 'Frutas', nota: 4.6 },
+    { id: '8', image: require('../assets/produto.jpg'), title: 'Banana', price: 'R$ 4,00', description: 'Banana prata, rica em potássio e ideal para um lanche saudável.', producer: 'Produtor K', category: 'Frutas', nota: 4.8 },
+    { id: '9', image: require('../assets/produto.jpg'), title: 'Cebola', price: 'R$ 5,50', description: 'Cebolas frescas, ideais para temperar e realçar o sabor das refeições.', producer: 'Produtor G', category: 'Vegetais', nota: 4.3 },
+    { id: '10', image: require('../assets/produto.jpg'), title: 'Batata', price: 'R$ 6,00', description: 'Batatas selecionadas, versáteis e perfeitas para diversas receitas.', producer: 'Produtor H', category: 'Vegetais', nota: 4.4 },
+    { id: '11', image: require('../assets/produto.jpg'), title: 'Cenoura', price: 'R$ 4,20', description: 'Cenouras frescas e crocantes, ricas em vitaminas e antioxidantes.', producer: 'Produtor I', category: 'Vegetais', nota: 4.5 },
+    { id: '12', image: require('../assets/produto.jpg'), title: 'Alface', price: 'R$ 2,50', description: 'Alface fresca e crocante, ideal para saladas e sanduíches.', producer: 'Produtor L', category: 'Vegetais', nota: 4.1 },
   ];
 
   const handleSearch = () => {
-    // Lógica para filtrar os produtos com base nos filtros
+    // Lógica para filtrar os produtos com base nos filtros e na consulta de pesquisa
     const filtered = allProducts.filter(item => {
       let isValid = true;
 
-      // preço
-      if (minPrice && parseFloat(item.price) < parseFloat(minPrice)) {
+      // Verifica se a consulta de pesquisa corresponde ao título do produto
+      if (searchQuery && !item.title.toLowerCase().includes(searchQuery.toLowerCase())) {
         isValid = false;
       }
-      if (maxPrice && parseFloat(item.price) > parseFloat(maxPrice)) {
+
+      // preço
+      if (minPrice && parseFloat(item.price.replace('R$', '').replace(',', '.')) < parseFloat(minPrice)) {
+        isValid = false;
+      }
+      if (maxPrice && parseFloat(item.price.replace('R$', '').replace(',', '.')) > parseFloat(maxPrice)) {
         isValid = false;
       }
 
       // categoria
       if (selectedCategory.length > 0 && !selectedCategory.includes(item.category)) {
-        isValid = false;
-      }
-
-      // localização
-      if (selectedLocation && !item.location.toLowerCase().includes(selectedLocation.toLowerCase())) {
         isValid = false;
       }
 
@@ -64,17 +72,19 @@ const ResultadoPesquisa = ({ route, navigation }) => {
 
   useEffect(() => {
     handleSearch();
-  }, [minPrice, maxPrice, selectedCategory, selectedLocation]);
+  }, [searchQuery, minPrice, maxPrice, selectedCategory]);
 
   const renderProductCard = ({ item }) => (
-    <View style={styles.cardContainer}>
-      <Image style={styles.cardImage} source={item.image} resizeMode="cover" />
-      <View style={styles.textContainer}>
-        <Text style={styles.cardTitle}>{item.title}</Text>
-        <Text style={styles.cardPrice}>R$ {item.price}</Text>
-        <Text style={styles.cardUser}>Vendido por: {item.user}</Text>
+    <TouchableOpacity onPress={() => navigation.navigate('ProdutoDetalhado', { product: item })}>
+      <View style={styles.cardContainer}>
+        <Image style={styles.cardImage} source={item.image} resizeMode="cover" />
+        <View style={styles.textContainer}>
+          <Text style={styles.cardTitle}>{item.title}</Text>
+          <Text style={styles.cardPrice}>{item.price}</Text>
+          <Text style={styles.cardUser}>Produtor: {item.producer}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 
   const handleCheckboxChange = (category, isChecked) => {
@@ -139,17 +149,22 @@ const ResultadoPesquisa = ({ route, navigation }) => {
                   isChecked={selectedCategory.includes('Grãos')}
                   onChange={() => handleCheckboxChange('Grãos', !selectedCategory.includes('Grãos'))}
                 />
+                <CustomCheckbox
+                  label="Frutas"
+                  isChecked={selectedCategory.includes('Frutas')}
+                  onChange={() => handleCheckboxChange('Frutas', !selectedCategory.includes('Frutas'))}
+                />
+                                <CustomCheckbox
+                  label="Vegetais"
+                  isChecked={selectedCategory.includes('Vegetais')}
+                  onChange={() => handleCheckboxChange('Vegetais', !selectedCategory.includes('Vegetais'))}
+                />
+                <CustomCheckbox
+                  label="Laticínios"
+                  isChecked={selectedCategory.includes('Laticínios')}
+                  onChange={() => handleCheckboxChange('Laticínios', !selectedCategory.includes('Laticínios'))}
+                />
               </View>
-
-              {/* Filtro de localização */}
-              <Text style={styles.filterSectionTitle}>Localização</Text>
-              <TextInput
-                style={styles.locationInput}
-                placeholder="Digite a cidade"
-                placeholderTextColor="#7c7c7c"
-                value={selectedLocation}
-                onChangeText={setSelectedLocation}
-              />
             </ScrollView>
 
             {/* Botão aplicar filtro */}
